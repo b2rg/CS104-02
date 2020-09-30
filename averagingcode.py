9834411b961523905c72aaecc00d212ebc14e57d
b2rg <67485369+b2rg@users.noreply.github.com>
2020-09-30 18:26:34 -0400

    averaging

---
+++ averagingcode.py
@@ -0,0 +1,13 @@
+#Accept the number of scores to average
+
+numberOfScores = int(input("Enter the value of your first score:"))
+
+numberOfScores2 = int(input("Enter the value of your second score:"))
+
+numberOfScores3 = int(input("Enter the value of your third score:"))
+
+average = (numberOfScores+numberOfScores2+numberOfScores3)/3
+
+print("average=",average)
+
+
