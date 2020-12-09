b90749d724e839fe016be7d669d61b766203489b
b2rg <67485369+b2rg@users.noreply.github.com>
2020-12-09 13:59:22 -0500

    Add files via upload

---
+++ NewPythonQueue.py
@@ -0,0 +1,24 @@
+names = (["Andrew", "Bob", "Charles", "Danielle", "Elizabeth", "Fred", "Gerald", "Henry","Isaac", "Jackelyn"])
+queue = names
+names.append("Andrew")
+names.append("Bob")
+names.append("Charles")
+names.append("Danielle")
+names.append("Elizabeth")
+names.append("Fred")
+names.append("Gerald")
+names.append("Henry")
+names.append("Isaac")
+names.append("Jackelyn")
+
+print(names)
+names.pop(0)
+print(names)
+
+len(names)
+print(names.pop(0))
+
+names = deque(["Andrew", "Bob", "Charles", "Danielle", "Elizabeth", "Fred", "Gerald", "Henry","Isaac", "Jackelyn"])
+print (names)
+
+
